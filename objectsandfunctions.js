718ef5b427ceaf8f5b993cade01cafd3bce98958
Katherine Mattison <kbmedisto@gmail.com>
2014-04-15 16:23:05 -0400

    first commit for jsobjectsandfunctions

---
+++ objectsandfunctions.js
@@ -0,0 +1,11 @@
+var katherine = new object()
+	katherine.living = true
+	katherine.age = 40
+	katherine.gender = female
+	katherine.doglover = true
+	katherine.aunt = true
+	katherine.married = false
+
+
+	// console.log (katherine)
+	// logs new object() values(living = true, age = 40, gender = female, doglover = true, aunt = true, married = false)
